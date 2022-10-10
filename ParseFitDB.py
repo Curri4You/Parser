ee98724609977438fb6c29003f5fb7f2b01d326a
HeeyoungJoe <heeyoungjoe2000@gmail.com>
2022-10-10 20:34:43 +0900

    index omitted

--- ParseFitDB.py
+++ ParseFitDB.py
@@ -49,7 +49,7 @@ def allmajorDB(all_curriculum):
         f.close()
     except:
         f=open('allmajor_filename.txt','a')
-        last='0'
+        last='\n0'
         f.write(last)
         f.close()
     
@@ -63,6 +63,7 @@ def allmajorDB(all_curriculum):
         university_name='이화여자대학교'
         college_name=general_info['college']
         major_name=general_info['div']
+        
         
         new_df=[subject_id,university_name,college_name,major_name]
         #print(new_df)
@@ -79,7 +80,7 @@ def allmajorDB(all_curriculum):
     모든 COURSE 정보를 긁어와야하므로, 모든 CURRICULUM을 종합해서 만들어야 한다. 
     ################
     필요한 것:course_id(학수번호) subject_id,professor(강의계획표 or 직접입력), is_english(불가),is_online(불가), credit, course_anme, syllabus_id(강의계획표)
-    파서에서 구할 수 있는 것: course_id(학수번호), subject_id, credit, course_name
+    파서에서 구할 수 있는 것: course_id(학수번호), subject_id, credit, course_name, 선수과목(이야기 안 나눔)
     
     general ifno: {'college': '엘텍공과대학', 'div': ' 소프트웨어학부', 'subject_name': ' 컴퓨터공학', 'type': 1, 'year': 2020, 'curr_id': 0}
     nav: (['구분', '영역명(학수번호)*:신설교과목', '교과목명', '이수권장학년', '설정학기', '시간', '학점', '필수여부', '학사편입생제외여부', '개설학과', '2022학년2학기개설여부', '비고'], [1, 2, 4, 8, 9, 11, 12, 13, 14, 16, 18, 19])
@@ -87,7 +88,7 @@ def allmajorDB(all_curriculum):
 '''
 def find_subject_id(name):
     try:#파일이 열리면
-        file=pd.read_csv('subject_id.csv')
+        file=pd.read_csv('subject_id.txt')
         
         #찾으면 
         retrieval=file[file['subject_name']==name]
@@ -98,11 +99,11 @@ def find_subject_id(name):
             new_id= int(file['subject_id'].iloc[-1]+1)
             new_row=pd.DataFrame([[name,new_id]],columns=['subject_name','subject_id'])
             file.append(new_row)
-            file.to_csv('subject_id.csv')
+            file.to_csv('subject_id.txt',index=False)
             return new_id
     except:
         file=pd.DataFrame([[name,0]],columns=['subject_name','subject_id'])
-        file.to_csv('subject_id.csv')
+        file.to_csv('subject_id.txt',index=False)
         return 0
     
     
@@ -120,9 +121,9 @@ def save_courselist(df,filename,filetype=0):
     #flush it in json
     #just yield 
     if filetype==0:
-        df.to_json('data\\course\\'+filename)
+        df.to_json('data\\course\\'+filename,index=False)
     elif filetype==1:
-        df.to_csv('data\\course\\'+filename)
+        df.to_csv('data\\course\\'+filename,index=False)
     
 def courseDB(all_curriculum):
     #목적: json파일로
@@ -136,7 +137,7 @@ def courseDB(all_curriculum):
         f.close()
     except:
         f=open('course_json_filename.txt','a')
-        last='0'
+        last='\n0'
         f.write(last)
         f.close()
     
@@ -152,16 +153,16 @@ def courseDB(all_curriculum):
         credit_index=indices[cols.index('학점')]-2
         course_name_index=indices[cols.index('교과목명')]-2
         course_id_index=indices[0]-2
+        is_open_index=indices[-1]-2
         
         #print(df.columns)
-        tmp=df.iloc[:,[course_id_index,subject_id_index,credit_index,course_name_index]]
+        tmp=df.iloc[:,[course_id_index,subject_id_index,is_open_index,credit_index,course_name_index]]
         prev_cols=tmp.columns
-        new_cols=['course_id','subject_id','credit','course_name']
+        new_cols=['course_id','subject_id','is_open','credit','course_name']
         rename_dict={p:n for p,n in zip(prev_cols,new_cols)}
         tmp=tmp.rename(columns=rename_dict)
         #tmp.rename(columns={course_id_index:'course_id',subject_id_index:'subject_id',credit_index:'credit',course_name_index:'course_name'},inplace=True)
         print(tmp)
-        #씨발 왜 하나만 되는 거야 
         tmp['subject_id']=tmp['subject_id'].map(find_subject_id)
         
         
@@ -195,7 +196,7 @@ if __name__=='__main__':
             #print(chosen['dfs'])
         
     allmajorDB(resultdicts)
-    #courseDB(resultdicts)
+    courseDB(resultdicts)
     #check json
     
     #print(pd.read_json('data\\major\\1.json'))
